e011bd25ca85340fb7d180f4085c2d415ec90cc4
adrukalski <ikslakurd@gmail.com>
2014-03-16 16:36:29 +0100

    add countLivingNeighbors method

--- src/GameOfLife.php
+++ src/GameOfLife.php
@@ -16,4 +16,44 @@ class GameOfLife
     {
         $this->grid = $grid;
     }
+
+    /**
+     * Counts the number of live neighbours.
+     *
+     * @param $xCoordinate
+     * @param $yCoordinate
+     *
+     * @return int Number of live neighbours
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function countLiveNeighbours($xCoordinate, $yCoordinate)
+    {
+        if (!is_int($xCoordinate) || !is_int($yCoordinate)) {
+            throw new \InvalidArgumentException("Coordinates must be integers");
+        }
+
+        $count = 0;
+
+        for ($i = ($xCoordinate - 1); $i <= ($xCoordinate + 1); $i++) {
+            if (!array_key_exists($i, $this->grid)) {
+                continue;
+            }
+
+            for ($j = ($yCoordinate - 1); $j <= ($yCoordinate + 1); $j++) {
+                if (!array_key_exists($j, $this->grid[$i])) {
+                    continue;
+                }
+
+                // current cell
+                if ($xCoordinate == $i && $yCoordinate == $j) {
+                    continue;
+                }
+
+                $count += (int) $this->grid[$i][$j];
+            }
+        }
+
+        return $count;
+    }
 }
--- tests/GameOfLifeTest.php
+++ tests/GameOfLifeTest.php
@@ -24,4 +24,41 @@ class GameOfLifeTest extends \PHPUnit_Framework_TestCase
 
         $this->assertInstanceOf('Ikslakurd\\GameOfLife\\GameOfLife', $gameOfLife);
     }
+
+    /**
+     * @covers \Ikslakurd\GameOfLife\GameOfLife::countLiveNeighbours
+     *
+     *  @expectedException \InvalidArgumentException
+     */
+    public function testExceptionIsRaisedForInvalidCountLiveNeighboursMethodArguments()
+    {
+        $grid = array();
+        $gameOfLife = new GameOfLife($grid);
+
+        $gameOfLife->countLiveNeighbours("foo", "bar");
+    }
+
+    /**
+     * @covers \Ikslakurd\GameOfLife\GameOfLife::countLiveNeighbours
+     */
+    public function testLiveNeightboursCanBeCounted()
+    {
+        $grid = array(
+            array(true, true, false),
+            array(false, true, false),
+            array(false, false, false)
+        );
+
+        $gameOfLife = new GameOfLife($grid);
+
+        $this->assertEquals(2, $gameOfLife->countLiveNeighbours(0, 0));
+        $this->assertEquals(2, $gameOfLife->countLiveNeighbours(0, 1));
+        $this->assertEquals(2, $gameOfLife->countLiveNeighbours(0, 2));
+        $this->assertEquals(3, $gameOfLife->countLiveNeighbours(1, 0));
+        $this->assertEquals(2, $gameOfLife->countLiveNeighbours(1, 1));
+        $this->assertEquals(2, $gameOfLife->countLiveNeighbours(1, 2));
+        $this->assertEquals(1, $gameOfLife->countLiveNeighbours(2, 0));
+        $this->assertEquals(1, $gameOfLife->countLiveNeighbours(2, 1));
+        $this->assertEquals(1, $gameOfLife->countLiveNeighbours(2, 2));
+    }
 }
