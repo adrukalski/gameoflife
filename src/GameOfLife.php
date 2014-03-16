3da5b5800a257d530322f7774abd60a179fb3053
adrukalski <ikslakurd@gmail.com>
2014-03-16 12:32:07 +0100

    add constructor

--- src/GameOfLife.php
+++ src/GameOfLife.php
@@ -8,4 +8,12 @@ class GameOfLife
      * @var array
      */
     protected $grid = array();
+
+    /**
+     * @param array $grid
+     */
+    public function __construct(array $grid)
+    {
+        $this->grid = $grid;
+    }
 }
--- tests/GameOfLifeTest.php
+++ tests/GameOfLifeTest.php
@@ -6,10 +6,21 @@ class GameOfLifeTest extends \PHPUnit_Framework_TestCase
 {
     /**
      * @covers \Ikslakurd\GameOfLife\GameOfLife::__construct
+     *
+     * @expectedException PHPUnit_Framework_Error
+     */
+    public function testErrorIsRaisedForInvalidConstructorArgument()
+    {
+        new GameOfLife();
+    }
+
+    /**
+     * @covers \Ikslakurd\GameOfLife\GameOfLife::__construct
      */
     public function testObjetCanBeConstructed()
     {
-        $gameOfLife = new GameOfLife();
+        $grid = array();
+        $gameOfLife = new GameOfLife($grid);
 
         $this->assertInstanceOf('Ikslakurd\\GameOfLife\\GameOfLife', $gameOfLife);
     }
